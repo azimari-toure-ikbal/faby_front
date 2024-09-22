05d3cfa52576fe307c493ad38790000fc3017127
Ikbal Azimari Touré <azimari.toure.etu@esmt.sn>
2024-09-22 08:33:54 +0000

    dep

--- js/ajout.js
+++ js/ajout.js
@@ -1,52 +1,67 @@
-const addUserForm = document.querySelector("form#add-user-form")
-addUserForm.addEventListener("submit", handleUserForm)
+const backendBaseUrl = "https://fasti-test-production.up.railway.app";
+
+const addUserForm = document.querySelector("form#add-user-form");
+addUserForm.addEventListener("submit", handleUserForm);
+
 function handleUserForm(event) {
-    event.preventDefault()
-    const data = new FormData(event.target)
-    const dataObject = Object.fromEntries(data.entries())
-    // console.log(dataObject)
-    const payload = {
-        num_etu : dataObject.num_etu,
-        nom : dataObject.user_nom,
-        prenom : dataObject.user_prenom,
-        email : dataObject.mail_etu,
-        mdp : dataObject.mdp_etu,
-        niveau : dataObject.user_level,
-        dob : new Date(dataObject.birth_etu),
-        creation : new Date()
-    }
-    console.log(payload)
-    fetch("http://127.0.0.1:8000/students/students", {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers:{
-            "Content-Type": "application/json"
-        }
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+
+  const payload = {
+    num_etu: dataObject.num_etu,
+    nom: dataObject.user_nom,
+    prenom: dataObject.user_prenom,
+    email: dataObject.mail_etu,
+    mdp: dataObject.mdp_etu,
+    niveau: dataObject.user_level,
+    dob: new Date(dataObject.birth_etu),
+    creation: new Date(),
+  };
+
+  fetch(`${backendBaseUrl}/students`, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(
+          "Vérifier les informations saisies ou contacter le support"
+        );
+      }
+      return res.json();
     })
+    .then(() => {
+      addUserForm.reset();
+      alert("L'étudiant à bien été ajouté");
+    })
+    .catch((error) => alert(error));
 }
 
-
-const addSubjectForm = document.querySelector("form#add-subject")
-addSubjectForm.addEventListener("submit", handleSubjectForm)
+const addSubjectForm = document.querySelector("form#add-subject");
+addSubjectForm.addEventListener("submit", handleSubjectForm);
 function handleSubjectForm(event) {
-    event.preventDefault()
-    const data = new FormData(event.target)
-    const dataObject = Object.fromEntries(data.entries())
-    // console.log(dataObject)
-    const payload = {
-        chemin : dataObject.img,
-        module : dataObject.module,
-        niveau : dataObject.subject-level,
-        enseignant : dataObject.prof,
-        annee_pub: dataObject.annee_pub,
-        creation : new Date()
-    }
-    console.log(payload)
-    fetch("http://127.0.0.1:8000/subjects/subjects", {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers:{
-            "Content-Type": "application/json"
-        }
-    })
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+  // console.log(dataObject)
+  const payload = {
+    chemin: dataObject.img,
+    module: dataObject.module,
+    niveau: dataObject.subject - level,
+    enseignant: dataObject.prof,
+    annee_pub: dataObject.annee_pub,
+    creation: new Date(),
+  };
+  console.log(payload);
+  fetch("http://127.0.0.1:8000/subjects/subjects", {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
--- js/auth.js
+++ js/auth.js
@@ -1,88 +1,98 @@
-// creation et recuperation cookie
-function setCookie(name, value, days){
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 *60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires +";path=/";
+window.onload = adminRedirection();
+
+const backendBaseUrl = "https://fasti-test-production.up.railway.app";
+
+// Creation de cookie
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
-// window.onload = adminredirection()
+// Recuperation de cookie
+function getCookie(name) {
+  let cookieArr = document.cookie.split(";");
 
-function getCookie(name){
-    let cookieArr = document.cookie.split(";")
+  for (let i = 0; i < cookieArr.length; i++) {
+    let cookiePair = cookieArr[i].split("=");
 
-    for(let i = 0; i < cookieArr.length; i++){
-        let cookiePair = cookieArr[i].split("=");
-
-        if(name === cookiePair[0].trim()){
-            return decodeURIComponent(cookiePair[1]);
-        }
+    if (name === cookiePair[0].trim()) {
+      return decodeURIComponent(cookiePair[1]);
     }
+  }
 
-    return null;
+  return null;
 }
 
-function adminredirection(){
-    const adminCookie = getCookie("admin")
-    if (adminCookie) {
-        window.location.href = "/admin/acceuil.html"
-    }
-
+// Suppression de cookie
+function deleteCookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
 
-const ConnectAdminForm = document.querySelector("form#admin-auth")
-ConnectAdminForm.addEventListener("submit", handleConnectAdminForm)
-function handleConnectAdminForm(event) {
-    event.preventDefault()
-    const data = new FormData(event.target)
-    const dataObject = Object.fromEntries(data.entries())
-    console.log(dataObject)
-    const payload = {
-        email : dataObject.email_admin,
-        password : dataObject.password_admin,
-    
+function adminRedirection() {
+  const adminCookie = getCookie("admin");
+  const pathname = window.location.pathname;
+
+  if (adminCookie && pathname === "/auth/admin.html") {
+    window.location.pathname = "/admin/acceuil.html";
+  }
 }
 
-// recup info auth form
-console.log(payload)
-fetch("https://fasti-test-production.up.railway.app/auth/admin", {
+const connectionAdmin = document.querySelector("form#admin-auth");
+connectionAdmin.addEventListener("submit", handleConnectionAdmin);
+
+function handleConnectionAdmin(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+  // console.log(dataObject)
+  const payload = {
+    email: dataObject.email_admin,
+    password: dataObject.password_admin,
+  };
+
+  // recup info auth form
+  //   console.log(payload);
+  fetch(`${backendBaseUrl}/auth/admin`, {
     method: "POST",
     body: JSON.stringify(payload),
-    headers:{
-        "Content-Type": "application/json"
-    }
-})
-.then((res) => res.json())
-.then((data) => {
-    setCookie("admin", JSON.stringify(data), 2)
-    window.location.replace("/admin/acceuil.html")
-    // console.log(data)
-    // console.log(document.cookie)
-})
-.catch((error) => alert(error))
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      console.log("res", res);
+      if (!res.ok) {
+        throw new Error("Vérifier votre email et votre mot de passe");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setCookie("admin", JSON.stringify(data), 2);
+      window.location.pathname = "/admin/acceuil.html";
+    })
+    .catch((error) => alert(error));
 }
 
-const ConnectUserForm = document.querySelector("form#user-auth")
-ConnectUserForm.addEventListener("submit", handleConnectUserForm)
-function handleConnectUserForm(event) {
-    event.preventDefault()
-    const data = new FormData(event.target)
-    const dataObject = Object.fromEntries(data.entries())
-    console.log(dataObject)
-    const payload = {
-        email : dataObject.email_admin,
-        mdp : dataObject.password_admin,
-    
-}
-console.log(payload)
-fetch("http://127.0.0.1:8000/users/users", {
+const connectionForm = document.querySelector("form#user-auth");
+connectionForm.addEventListener("submit", handleConnectionForm);
+
+function handleConnectionForm(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+  // console.log(dataObject)
+  const payload = {
+    email: dataObject.email_admin,
+    mdp: dataObject.password_admin,
+  };
+  console.log(payload);
+  fetch(`${backendBaseUrl}/users/users`, {
     method: "POST",
     body: JSON.stringify(payload),
-    headers:{
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-})
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
 }
-
-
-
