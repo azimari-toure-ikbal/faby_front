c486d9672671502854bf8d054f12766ead527249
Ikbal Azimari Touré <azimari.toure.etu@esmt.sn>
2024-09-26 15:24:18 +0000

    added tables

---
+++ js/suivie.js
@@ -0,0 +1,62 @@
+const backendBaseUrl = "https://fasti-test-production.up.railway.app";
+
+function getStudents() {
+  fetch(`${backendBaseUrl}/students`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Erreur lors de la récupération des étudiants");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const studentsTable = document.querySelector(
+        "table#students-table>tbody"
+      );
+      data.forEach((student) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${student.nom}</td>
+          <td>${student.prenom}</td>
+          <td>${student.num_etu}</td>
+          <td>${student.date_naiss}</td>
+          <td>${student.mail}</td>
+          <td>${student.niveau}</td>
+          <td>
+            <button id="supp" class="btn-outline">Supprimer</button>
+          </td>
+        `;
+        studentsTable.appendChild(row);
+      });
+    })
+    .catch((error) => alert(error));
+}
+
+function getSubjects() {
+  fetch(`${backendBaseUrl}/subjects`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Erreur lors de la récupération des sujets");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const subjectsTable = document.querySelector(
+        "table#subjects-table>tbody"
+      );
+      data.forEach((subject) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+          <td>${subject.title}</td>
+          <td>${subject.module}</td>
+          <td>${subject.niveau}</td>
+          <td>${subject.enseignant}</td>
+          <td>${subject.annee_pub}</td>
+          <td>
+            <button id="supp" class="btn-outline">Supprimer</button>
+          </td>
+        `;
+        subjectsTable.appendChild(row);
+      });
+    })
+    .catch((error) => alert(error));
+}
