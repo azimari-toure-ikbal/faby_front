9494a66f39924d267d1ac6c68d15465ec0207859
Ikbal Azimari Touré <azimari.toure.etu@esmt.sn>
2024-09-26 14:31:05 +0000

    added reply form

--- js/discussion.js
+++ js/discussion.js
@@ -1,3 +1,17 @@
+function getCookie(name) {
+  let cookieArr = document.cookie.split(";");
+
+  for (let i = 0; i < cookieArr.length; i++) {
+    let cookiePair = cookieArr[i].split("=");
+
+    if (name === cookiePair[0].trim()) {
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+
+  return null;
+}
+
 function getQueryParam(param) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param);
@@ -32,10 +46,6 @@ function getDiscussion(id) {
       `;
 
       const messages = document.querySelector(".reponses-section");
-      //   There's a h3 tag inside the response section div
-      // secion.responses-section > h3 > div.response
-
-      // Create me the div.response for each message
       data.messages.forEach((message) => {
         const response = document.createElement("div");
         response.classList.add("reponse");
@@ -53,37 +63,27 @@ function getDiscussion(id) {
     .catch((error) => alert(error));
 }
 
-function getCookie(name) {
-  let cookieArr = document.cookie.split(";");
-
-  for (let i = 0; i < cookieArr.length; i++) {
-    let cookiePair = cookieArr[i].split("=");
-
-    if (name === cookiePair[0].trim()) {
-      return decodeURIComponent(cookiePair[1]);
-    }
-  }
-
-  return null;
-}
-
 const messageForm = document.querySelector("form#message-form");
 messageForm.addEventListener("submit", handleMessageForm);
 
 function handleMessageForm(event) {
   event.preventDefault();
+  const auth = getCookie("user");
 
-  if (!getCookie("user")) {
+  if (!auth) {
     alert("Vous devez être connecté pour envoyer un message");
     return;
   }
+
+  const user = JSON.parse(auth);
 
   const data = new FormData(event.target);
   const dataObject = Object.fromEntries(data.entries());
 
   const payload = {
-    message: dataObject.message,
-    discussion_id: dataObject.discussion_id,
+    contenu: dataObject.message,
+    id_discussion: dataObject.discussion_id,
+    id_utilisateur: user.id,
   };
 
   fetch(`${backendBaseUrl}/messages`, {
@@ -101,6 +101,18 @@ function handleMessageForm(event) {
     })
     .then((data) => {
       {
+        const messages = document.querySelector(".reponses-section");
+        const response = document.createElement("div");
+        response.classList.add("reponse");
+        response.innerHTML = `
+          <p class="reponse-author">${user.prenom} ${user.nom} - ${new Date(
+          data.creation
+        ).toLocaleString()}</p>
+          <div class="reponse-message">
+            <p>${dataObject.message}</p>
+          </div>
+        `;
+        messages.appendChild(response);
       }
     })
     .catch((error) => alert(error));
